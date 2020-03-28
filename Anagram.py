bf82e3045e3bae81bd65c5e4dc9c09f5511dd228
unhingedpotter <unhingedpotter@gmail.com>
2020-03-28 23:42:19 +0530

    Added a. Throttel and  debounce b. CommonElemArr query

--- Anagram.py
+++ Anagram.py
@@ -1,82 +1,142 @@
-# Python program to check whether two strings are 
-# anagrams of each other 
+# Python program to check whether two strings are
+# anagrams of each other
 
-# function to check whether two strings are anagram 
-# of each other 
-def areAnagram(str1, str2): 
-	# Get lengths of both strings 
-	n1 = len(str1) 
-	n2 = len(str2) 
-
-	# If lenght of both strings is not same, then 
-	# they cannot be anagram 
-	if n1 != n2: 
-		return 0
-
-	# Sort both strings 
-	str1 = sorted(str1) 
-	str2 = sorted(str2) 
-
-	# Compare sorted strings 
-	for i in range(0, n1): 
-		if str1[i] != str2[i]: 
-			return 0
-
-	return 1
+# function to check whether two strings are anagram
+# of each other
 
 
-# Driver program to test the above function 
+def areAnagram(str1, str2):
+    # Get lengths of both strings
+    n1 = len(str1)
+    n2 = len(str2)
+
+    # If lenght of both strings is not same, then
+    # they cannot be anagram
+    if n1 != n2:
+        return 0
+
+    # Sort both strings
+    str1 = sorted(str1)
+    str2 = sorted(str2)
+
+    # Compare sorted strings
+    for i in range(0, n1):
+        if str1[i] != str2[i]:
+            return 0
+
+    return 1
+
+
+# Driver program to test the above function
 str1 = "test"
 str2 = "ttew"
-if areAnagram(str1, str2): 
-	print ("The two strings are anagram of each other") 
-else: 
-	print ("The two strings are not anagram of each other") 
+if areAnagram(str1, str2):
+    print("The two strings are anagram of each other")
+else:
+    print("The two strings are not anagram of each other")
 
-# This code is contributed by Bhavya Jain 
+# This code is contributed by Bhavya Jain
 
 
 ##################################################################################
 
-# Python program to check if two strings are anagrams of 
-# each other 
+# Python program to check if two strings are anagrams of
+# each other
 NO_OF_CHARS = 256
 
-# Function to check whether two strings are anagram of 
-# each other 
-def areAnagram(str1, str2): 
+# Function to check whether two strings are anagram of
+# each other
 
-	# Create two count arrays and initialize all values as 0 
-	count1 = [0] * NO_OF_CHARS 
-	count2 = [0] * NO_OF_CHARS 
 
-	# For each character in input strings, increment count 
-	# in the corresponding count array 
-	for i in str1: 
-		count1[ord(i)]+= 1
+def areAnagram(str1, str2):
 
-	for i in str2: 
-		count2[ord(i)]+= 1
+    # Create two count arrays and initialize all values as 0
+    count1 = [0] * NO_OF_CHARS
+    count2 = [0] * NO_OF_CHARS
 
-	# If both strings are of different length. Removing this 
-	# condition will make the program fail for strings like 
-	# "aaca" and "aca" 
-	if len(str1) != len(str2): 
-		return 0
+    # For each character in input strings, increment count
+    # in the corresponding count array
+    for i in str1:
+        count1[ord(i)] += 1
 
-	# Compare count arrays 
-	for i in xrange(NO_OF_CHARS): 
-		if count1[i] != count2[i]: 
-			return 0
+    for i in str2:
+        count2[ord(i)] += 1
 
-	return 1
+    # If both strings are of different length. Removing this
+    # condition will make the program fail for strings like
+    # "aaca" and "aca"
+    if len(str1) != len(str2):
+        return 0
 
-# Driver program to test the above functions 
+    # Compare count arrays
+    for i in xrange(NO_OF_CHARS):
+        if count1[i] != count2[i]:
+            return 0
+
+    return 1
+
+
+# https://medium.com/human-in-a-machine-world/quicksort-the-best-sorting-algorithm-6ab461b5a9d0
+
+# Driver program to test the above functions
 str1 = "geeksforgeeks"
 str2 = "forgeeksgeeks"
-if areAnagram(str1, str2): 
-	print "The two strings are anagram of each other"
-else: 
-	print "The two strings are not anagram of each other"
+if areAnagram(str1, str2):
+    print "The two strings are anagram of each other"
+else:
+    print "The two strings are not anagram of each other"
 
-# This code is contributed by Bhavya Jain 
+# This code is contributed by Bhavya Jain
+
+
+# Python program for implementation of Quicksort Sort
+
+# This function takes last element as pivot, places
+# the pivot element at its correct position in sorted
+# array, and places all smaller (smaller than pivot)
+# to left of pivot and all greater elements to right
+# of pivot
+def partition(arr, low, high):
+    i = (low-1)		 # index of smaller element
+    pivot = arr[high]	 # pivot
+
+    for j in range(low, high):
+
+        # If current element is smaller than the pivot
+        if arr[j] < pivot:
+
+            # increment index of smaller element
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low --> Starting index,
+# high --> Ending index
+
+# Function to do Quick sort
+
+
+def quickSort(arr, low, high):
+    if low < high:
+
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+
+
+# Driver code to test above
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
