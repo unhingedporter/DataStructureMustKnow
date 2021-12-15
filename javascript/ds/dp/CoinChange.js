eb08f546b9cffafe1cd9bdba40d132497317b826
unhignedporter <unhingedporter@gmail.com>
2021-12-15 17:11:07 +0530

    Added DP

--- javascript/ds/dp/CoinChange.js
+++ javascript/ds/dp/CoinChange.js
@@ -1,10 +1,10 @@
 // do not look into it :(
 
 class CoinChange {
-    constructor() {}
+    constructor() { }
 
     // [2,3,5] // 7 
-    findPathAndCount(arr_coins, num_coins, sum, amountSoFar){
+    findPathAndCount(arr_coins, num_coins, sum, amountSoFar) {
 
 
 
@@ -18,7 +18,7 @@ class CoinChange {
         if (num_coins === 0) {
             return 0;
         }
- 
+
         var result = this.findPossibleCount(arr_coins, num_coins - 1, sum);
         console.log(`2. The number of coins reached ${result}`);
         if (arr_coins[num_coins - 1] <= sum) {
@@ -30,6 +30,36 @@ class CoinChange {
         }
         return result;
     }
+
+    /* Dynamic Programming javascript implementation of Coin
+    Change problem */
+
+    countWays(S, m, n) {
+        //Time complexity of this function: O(mn)
+        //Space Complexity of this function: O(n)
+
+        // table[i] will be storing the number of solutions
+        // for value i. We need n+1 rows as the table is
+        // constructed in bottom up manner using the base
+        // case (n = 0)
+        // Initialize all table values as 0
+        //O(n)
+        var table = Array(n + 1).fill(0);
+
+
+        // Base case (If given value is 0)
+        table[0] = 1;
+
+        // Pick all coins one by one and update the table
+        // values after the index greater than or equal to
+        // the value of the picked coin
+        for (i = 0; i < m; i++)
+            for (j = S[i]; j <= n; j++)
+                table[j] += table[j - S[i]];
+
+        return table[n];
+    }
+
 }
 
 var cc = new CoinChange();
@@ -51,79 +81,79 @@ console.log(cc.findPossibleCount([5, 3, 6, 2], 50, 10));
 // 5, 3, 6, 2 | xx | 10
 
 
-var coinChange = function(coins, amount) {
-    
+var coinChange = function (coins, amount) {
+
     var res = 0;
-    var countCoinChange = function(coins, amount, n){
-    
-        if(amount === 0){
+    var countCoinChange = function (coins, amount, n) {
+
+        if (amount === 0) {
             return 1;
         }
-        
-        if(n === 0){
+
+        if (n === 0) {
             return 0;
         }
 
         res = res + countCoinChange(coins, amount, n - 1);
 
-        if(coins[n - 1]<= amount){
-            res = res + countCoinChange(coins, amount - coins[n], n );
+        if (coins[n - 1] <= amount) {
+            res = res + countCoinChange(coins, amount - coins[n], n);
         }
-        
-        return res ;
+
+        return res;
 
     }
-    
+
     countCoinChange(coins, amount, coins.length - 1);
-    return res=== 0 ? -1: res;    
+    return res === 0 ? -1 : res;
 };
 
 
- 
-var coinChange = function(coins, amount) {
-    
-    coins = coins.sort((p,q)=> p - q);
+
+var coinChange = function (coins, amount) {
+
+    coins = coins.sort((p, q) => p - q);
     var len = coins.length;
     var dp = [];
     dp.length = amount + 1;
     dp.fill(+Infinity);
     dp[0] = 0;
-    
-    for(var i = 1; i <= amount; i++){
-        for(var j =0; j < len; j++){
-            if(coins[j] > i) {
+
+    for (var i = 1; i <= amount; i++) {
+        for (var j = 0; j < len; j++) {
+            if (coins[j] > i) {
                 continue;
             }
             dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
         }
     }
-    
+
     console.log(dp);
-    return dp[amount] === +Infinity ? -1 : dp[amount];   
-    
+    return dp[amount] === +Infinity ? -1 : dp[amount];
+
 }
 coinChange([5, 3, 6, 2], 10)
 
-var coinChange = function(coins, amount) {
-    
-    coins = coins.sort((p,q)=> p - q);
+var coinChange = function (coins, amount) {
+
+    coins = coins.sort((p, q) => p - q);
     var len = coins.length;
     var dp = [];
     dp.length = amount + 1;
     dp.fill(+Infinity);
     dp[0] = 0;
-    
-    for(var i = 1; i <= amount; i++){
-        for(var j =0; j < len; j++){
-            if(coins[j] > i) {
+
+    for (var i = 1; i <= amount; i++) {
+        for (var j = 0; j < len; j++) {
+            if (coins[j] > i) {
                 continue;
             }
-            dp[i] = Math.min(dp[i], 1 +  dp[i - coins[j]]);
+            dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
         }
     }
-    
+
     console.log(dp);
-    return dp[amount] === +Infinity ? -1 : dp[amount];   
-    
+    return dp[amount] === +Infinity ? -1 : dp[amount];
+
 }
 coinChange([5, 3, 6, 2], 10)
--- javascript/ds/dp/goldmine.js
+++ javascript/ds/dp/goldmine.js
@@ -1,79 +1,99 @@
+// Given a gold mine of n*m dimensions. 
+//Each field in this mine contains a positive integer which is the amount of gold in tons. 
+// Initially the miner is at first column but can be at any row. 
+// He can move only (right->,right up /,right down\) that is from a given cell, the miner can move to the cell diagonally up towards the right or right or diagonally down towards the right. Find out maximum amount of gold he can collect. 
+// Examples: 
+ 
 
-	// JavaScript program to solve Gold Mine problem
-	
-	let MAX = 100;
-	
-	// Returns maximum amount of gold that
-	// can be collected when journey started
-	// from first column and moves allowed
-	// are right, right-up and right-down
-	function getMaxGold(gold, m, n)
-	{
-		
-		// Create a table for storing
-		// intermediate results and initialize
-		// all cells to 0. The first row of
-		// goldMineTable gives the maximum
-		// gold that the miner can collect
-		// when starts that row
-		let goldTable = new Array(m);
-		
-		for(let i = 0; i < m; i++)
-		{
-			goldTable[i] = new Array(n);
-			for(let j = 0; j < n; j++)
-			{
-				goldTable[i][j] = 0;
-			}
-		}
-	
-		for (let col = n-1; col >= 0; col--)
-		{
-			for (let row = 0; row < m; row++)
-			{
-				
-				// Gold collected on going to
-				// the cell on the right(->)
-				let right = (col == n-1) ? 0
-						: goldTable[row][col+1];
-	
-				// Gold collected on going to
-				// the cell to right up (/)
-				let right_up = (row == 0 ||
-							col == n-1) ? 0 :
-						goldTable[row-1][col+1];
-	
-				// Gold collected on going to
-				// the cell to right down (\)
-				let right_down = (row == m-1
-							|| col == n-1) ? 0 :
-						goldTable[row+1][col+1];
-	
-				// Max gold collected from taking
-				// either of the above 3 paths
-				goldTable[row][col] = gold[row][col]
-				+ Math.max(right, Math.max(right_up,
-									right_down));
-														
-			}
-		}
-	
-		// The max amount of gold collected will be
-		// the max value in first column of all rows
-		let res = goldTable[0][0];
-		
-		for (let i = 1; i < m; i++)
-			res = Math.max(res, goldTable[i][0]);
-			
-		return res;
-	}
-	
-	let gold = [ [1, 3, 1, 5],
-				[2, 2, 4, 1],
-				[5, 0, 2, 3],
-				[0, 6, 1, 2] ];
-						
-	let m = 4, n = 4;
+// Input : mat[][] = {{1, 3, 3},
+//                    {2, 1, 4},
+//                   {0, 6, 4}};
+// Output : 12 
+// {(1,0)->(2,1)->(1,2)}
 
-	console.log(getMaxGold(gold, m, n));
+// Input: mat[][] = { {1, 3, 1, 5},
+//                    {2, 2, 4, 1},
+//                    {5, 0, 2, 3},
+//                    {0, 6, 1, 2}};
+// Output : 16
+// (2,0) -> (1,1) -> (1,2) -> (0,3) OR
+// (2,0) -> (3,1) -> (2,2) -> (2,3)
+
+// Input : mat[][] = {{10, 33, 13, 15},
+//                   {22, 21, 04, 1},
+//                   {5, 0, 2, 3},
+//                   {0, 6, 14, 2}};
+// Output : 83
+// JavaScript program to solve Gold Mine problem
+
+let MAX = 100;
+
+// Returns maximum amount of gold that
+// can be collected when journey started
+// from first column and moves allowed
+// are right, right-up and right-down
+function getMaxGold(gold, m, n) {
+
+    // Create a table for storing
+    // intermediate results and initialize
+    // all cells to 0. The first row of
+    // goldMineTable gives the maximum
+    // gold that the miner can collect
+    // when starts that row
+    let goldTable = new Array(m);
+
+    for (let i = 0; i < m; i++) {
+        goldTable[i] = new Array(n);
+        for (let j = 0; j < n; j++) {
+            goldTable[i][j] = 0;
+        }
+    }
+
+    for (let col = n - 1; col >= 0; col--) {
+        for (let row = 0; row < m; row++) {
+
+            // Gold collected on going to
+            // the cell on the right(->)
+            let right = (col == n - 1) ? 0
+                : goldTable[row][col + 1];
+
+            // Gold collected on going to
+            // the cell to right up (/)
+            let right_up = (row == 0 ||
+                col == n - 1) ? 0 :
+                goldTable[row - 1][col + 1];
+
+            // Gold collected on going to
+            // the cell to right down (\)
+            let right_down = (row == m - 1
+                || col == n - 1) ? 0 :
+                goldTable[row + 1][col + 1];
+
+            // Max gold collected from taking
+            // either of the above 3 paths
+            goldTable[row][col] = gold[row][col]
+                + Math.max(right, Math.max(right_up,
+                    right_down));
+
+        }
+    }
+
+    // The max amount of gold collected will be
+    // the max value in first column of all rows
+    let res = goldTable[0][0];
+
+    for (let i = 1; i < m; i++)
+        res = Math.max(res, goldTable[i][0]);
+
+    return res;
+}
+
+let gold = [[1, 3, 1, 5],
+[2, 2, 4, 1],
+[5, 0, 2, 3],
+[0, 6, 1, 2]];
+
+let m = 4, n = 4;
+
+console.log(getMaxGold(gold, m, n));
 
